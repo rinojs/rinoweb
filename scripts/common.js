46c2c842cac3f3a3c968e91b3346c7ba5ac5fd18
Victor Chanil Park <opdev1004@gmail.com>
2024-11-24 02:04:02 +0900

    new website

---
+++ scripts/common.js
@@ -0,0 +1,1 @@
+!function(){"use strict";document.addEventListener("DOMContentLoaded",(function(){const e=document.getElementById("sidebar-toggle"),t=document.getElementById("sidebar-wrapper");e.addEventListener("click",(function(){const e="block"===t.style.display;t.style.display=e?"none":"block",console.log("asdas")}))}))}();
